a6c83745f3cd422bc7f7bb295d08371414d0afb0
Elie G <elyahou.hadass@gmail.com>
2024-12-14 20:54:14 +0200

    Set theme and color scheme meta tags dynamically
    Added logic to dynamically update the "color-scheme" and "theme-color" meta tags based on the current color mode. This ensures proper theming support for both light and dark modes. The implementation includes utility functions to handle color conversions and DOM updates.

--- site/src/jsMain/kotlin/com/kdroid/blog/components/layouts/PageLayout.kt
+++ site/src/jsMain/kotlin/com/kdroid/blog/components/layouts/PageLayout.kt
@@ -1,21 +1,27 @@
 package com.kdroid.blog.components.layouts
 
 import androidx.compose.runtime.*
+import com.kdroid.blog.components.sections.Footer
+import com.kdroid.blog.components.sections.NavHeader
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.ColumnScope
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Color
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.silk.components.text.SpanText
 import com.varabyte.kobweb.silk.style.CssStyle
 import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
 import com.varabyte.kobweb.silk.style.toAttrs
-import com.kdroid.blog.components.sections.Footer
-import com.kdroid.blog.components.sections.NavHeader
+import com.varabyte.kobweb.silk.theme.colors.ColorMode
+import com.varabyte.kobweb.silk.theme.colors.palette.background
+import com.varabyte.kobweb.silk.theme.colors.palette.toPalette
 import kotlinx.browser.document
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
+import org.w3c.dom.Element
+import org.w3c.dom.get
 
 val CenteredSectionStyle = CssStyle {
     base { Modifier.fillMaxWidth(90.percent) }
@@ -30,6 +36,44 @@ fun PageLayout(title: String, description: String = "Tech chatter, tutorials, an
         document.title = "$title"
         document.querySelector("""meta[name="description"]""")!!.setAttribute("content", description)
     }
+
+    val colorMode = ColorMode.current
+    LaunchedEffect(colorMode) {
+        fun Color.Rgb.toHexString(): String {
+            // Convert each color value to a two-character hex string
+            val red = red.coerceIn(0, 255).toString(16).padStart(2, '0')
+            val green = green.coerceIn(0, 255).toString(16).padStart(2, '0')
+            val blue = blue.coerceIn(0, 255).toString(16).padStart(2, '0')
+
+            // Combine them into a single hex color code
+            return "#$red$green$blue"
+        }
+
+        val colorSchemeElement = document.querySelector("""meta[name="color-scheme"]""")
+        if (colorSchemeElement == null) {
+            document.createElement("meta").apply {
+                setAttribute("name", "color-scheme")
+                setAttribute("content", "light dark")
+            }.also { document.head!!.appendChild(it) }
+        }
+
+        var themeColorElements = document.querySelectorAll("""meta[name="theme-color"]""")
+        if (themeColorElements.length == 0) {
+            listOf("light", "dark").forEach { mode ->
+                document.createElement("meta").apply {
+                    setAttribute("name", "theme-color")
+                    setAttribute("media", "(prefers-color-scheme: $mode)")
+                }.also { document.head!!.appendChild(it) }
+            }
+            themeColorElements = document.querySelectorAll("""meta[name="theme-color"]""")
+        }
+
+        for (i in 0 until themeColorElements.length) {
+            val element = (themeColorElements[i] as Element)
+            element.setAttribute("content", colorMode.toPalette().background.toRgb().toHexString())
+        }
+    }
+
 
     Box(Modifier
         .fillMaxWidth()
